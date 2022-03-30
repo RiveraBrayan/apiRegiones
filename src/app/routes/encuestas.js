04e161f9c38d8b9678f6201054553d9cffd1011b
Anson Arroyo <57742199+AnsonJahaziel@users.noreply.github.com>
2022-03-30 11:56:55 -0700

    Corregido detalles

--- src/app/routes/encuestas.js
+++ src/app/routes/encuestas.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
 
   // GET DATA
   app.get("/encuesta", (req, res) => {
-     connection.query("SELECT * FROM pruebas.pruebaapi", (err, rows, fields) => {
+    connection.query("SELECT * FROM pruebas.pruebaapi", (err, rows, fields) => {
       if (!err) {
         res.json(rows);
       } else {
@@ -18,14 +18,14 @@ module.exports = (app) => {
   // GET DATA WITH ID
   app.get("/encuesta/:id", (req, res) => {
     const { id } = req.params;
-     connection.query(
+    connection.query(
       "SELECT * FROM pruebas.pruebaapi WHERE idpruebaApi = ?",
       [id],
       (err, rows, fields) => {
         if (rows != '') {
           res.json(rows[0]);
         } else {
-          console.log("Not Found" );
+          console.log("Not Found");
         }
       }
     );
@@ -34,7 +34,7 @@ module.exports = (app) => {
   // DELETE DATA
   app.delete("/encuesta/:id", (req, res) => {
     const { id } = req.params;
-     connection.query(
+    connection.query(
       "DELETE FROM pruebas.pruebaapi WHERE idpruebaApi = ?",
       [id],
       (err, rows, fields) => {
@@ -49,28 +49,28 @@ module.exports = (app) => {
 
   // INSERT DATA
   app.post("/encuesta", (req, res) => {
-    const { valor1, valor2,valor3 } = req.body;
+    const { valor1, valor2, valor3 } = req.body;
     console.log(valor1, valor2, valor3);
 
     const query = ` INSERT INTO pruebas.pruebaapi SET valor1 = ` + "'" + valor1 + "'" + ' , valor2 = ' + "'" + valor2 + "'" + ' , valor3 =' + "'" + valor3 + "'";
     // res.json({ query });
 
-     connection.query(query, (err, rows, fields) => {
+    connection.query(query, (err, rows, fields) => {
       if (!err) {
         res.json({ status: "Data Saved" });
       } else {
         console.log(err);
       }
     });
-   }); 
+  });
 
-   //UPDATE DATA
+  //UPDATE DATA
   app.put("/encuesta/:id", (req, res) => {
 
-    const { valor1, valor2,valor3 } = req.body;
-    const idpruebaApi =  req.params.id;
+    const { valor1, valor2, valor3 } = req.body;
+    const idpruebaApi = req.params.id;
     const query = `  UPDATE pruebas.pruebaapi SET valor1 = ` + "'" + valor1 + "'" + ' , valor2 = ' + "'" + valor2 + "'" + ' , valor3 =' + "'" + valor3 + "'" + ' WHERE idpruebaApi = ' + idpruebaApi;
-     connection.query(query, (err, rows, fields) => {
+    connection.query(query, (err, rows, fields) => {
       if (!err) {
         res.json({ status: "Data Updated" });
       } else {
@@ -85,22 +85,26 @@ module.exports = (app) => {
   //INSERT DATA WITH JSON DOCUMENT
   app.post("/testing", (req, res) => {
     const datos = req.body;
+
     for (let i = 0; i < datos.length; i++) {
-          const data = JSON.parse(datos[i]);
-          const query = ` INSERT INTO pruebas.pruebaapi SET valor1 = ` + "'" + data.valor1 + "'" + ' , valor2 = ' + "'" + data.valor2 + "'" + ' , valor3 =' + "'" + data.valor3 + "'";
-        connection.query(query, (err, rows) => {
-          if (!err) {
-            console.log(res);
-          } else { 
-            console.log(err);
-          }
-        });
 
-        console.log(res);
+      const dato = JSON.parse(datos[i]);
+
+      const query = ` INSERT INTO pruebas.pruebaapi SET valor1 = ` + "'" + dato.valor1 + "'" + ' , valor2 = ' + "'" + dato.valor2 + "'" + ' , valor3 =' + "'" + dato.valor3 + "'" + ' , valor4 =' + "'" + dato.valor4 + "'"  + ' , valor5 =' + "'" + dato.valor5 + "'";
+      connection.query(query, (err, rows) => {
+        if (!err) {
+          console.log("Data Saved");
+        } else {
+          console.log(err);
+        }
+       
+      });
+      console.log(query);
     }
-    
-   }); 
-
-
+    res.json(200);
+  });
 
 };
+
+
+
--- src/config/server.js
+++ src/config/server.js
@@ -5,6 +5,13 @@ const cors = require('cors')
 
 const app = express();
 app.use(cors());
+
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
